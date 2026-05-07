ff8c95ac895d2cdc422b6e05fd166a7c9e8a7c41
Fauzi <fauzijuventini@gmail.com>
2026-05-07 14:08:47 +0700

    feat(cors): enable HTTPS support for production origin
    Add "https://banding.prabskie.my.id" to the allowed CORS origins list in the backend to support secure connections in production environments.

--- backend/main.py
+++ backend/main.py
@@ -60,7 +60,8 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173", 
         "http://127.0.0.1:5173",
-        "http://banding.prabskie.my.id"
+        "http://banding.prabskie.my.id",
+        "https://banding.prabskie.my.id"
     ],
     allow_credentials=True,
     allow_methods=["*"],
